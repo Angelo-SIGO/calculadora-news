b6cb849dd739f34ae4f5d224e07497c7eec30167
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2024-01-02 16:04:21 -0300

    build: add initial project setup
    The initial configuration of the project was done by adding support for conventional commits, with
    commitizen, and support for React.JS with Babel, Webpack and SASS.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { URL } from "url";
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+export default {
+    mode: 'development',
+    entry: `${__dirname}/src/main.js`,
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: `${__dirname}/node_modules`,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `${__dirname}/public/index.html`
+        })
+    ],
+    devServer: {
+        static: {
+            directory: `${__dirname}/dist`
+        },
+        hot: true,
+        open: true
+    }
+};
