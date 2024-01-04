7e79c3b5d58cdc0f2c09cc9f08998cc6027fceea
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2024-01-04 20:44:42 -0300

    feat(steps counter): add steps counter component

--- src/components/layouts/Section.jsx
+++ src/components/layouts/Section.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import StepsCounter from "../form/StepsCounter";
 
 export default function Section({ id, children }) {
     return <section id={id} className="card-section">{children}</section>
@@ -7,7 +8,8 @@ export default function Section({ id, children }) {
 export function MainSection() {
     return (
         <Section id="main">
-
+            <h1>Calculadora NEWS</h1>
+            <SectionBody/>
         </Section>
     )
 }
@@ -17,5 +19,15 @@ export function DisplaySection() {
         <Section id="display">
             
         </Section>
+    )
+}
+
+function SectionBody() {
+    return (
+        <div className="card-section__body">
+            <section className="card-section__body-content">
+                <StepsCounter steps={2} />
+            </section>
+        </div>
     )
 }
