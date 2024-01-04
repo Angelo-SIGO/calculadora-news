409b11ad4ede6686f44bec89a0e9f322ceb53464
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2024-01-04 14:58:04 -0300

    refactor(styles): reorganize style files
    The structure of style files (scss) has been changed to improve the organization of the project and
    optimize the productivity of the development team.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import NumberField from "./components/inputs/InputField";
 
 export default function App() {
-
+    return <NumberField id="temperature" icon="right">&#10004;</NumberField>
 }
