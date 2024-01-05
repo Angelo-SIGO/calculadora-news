e9b404fc7dc6b5e166ace418408b1c4c1e8695ed
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2024-01-05 11:41:00 -0300

    feat: add score calculation for each field

--- src/components/inputs/InputField.jsx
+++ src/components/inputs/InputField.jsx
@@ -19,25 +19,93 @@ function determineState(hasNumbers) {
 function NumberField({ id, onScoreChange }) {
     const [value, setValue] = useState("");
     const [state, setState] = useState(FIELD_STATES.NONE);
-    const [score, setScore] = useState(0);
+    const [temperatureScore, setTemperatureScore] = useState(0);
+    const [heartRateScore, setHeartRateScore] = useState(0);
+    const [bloodPressureScore, setBloodPressureScore] = useState(0);
+    const [breathRateScore, setBreathRateScore] = useState(0);
+    const [oxygenSaturationScore, setOxygenSaturationScore] = useState(0);
 
     function calculateTemperatureScore(value) {
         if (value <= 35) {
-            setScore(3);
+            return 3;
         } else if (value <= 36 && value > 35) {
-            setScore(2);
+            return 2;
         } else if (value > 36 && value <= 38) {
-            setScore(1);
+            return 1;
         } else if (value > 38 && value < 39) {
-            setScore(2);
+            return 2;
         } else {
-            setScore(3);
+            return 3;
         }
     }
 
+    // Função para calcular a pontuação da Frequência Cardíaca
+    function calculateHeartRateScore(value) {
+        if (value <= 50 || value >= 101) {
+            return 3;
+        } else if ((value >= 41 && value <= 50) || (value >= 91 && value <= 100)) {
+            return 2;
+        } else if ((value >= 51 && value <= 90)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Função para calcular a pontuação da Pressão Arterial Sistólica
+    function calculateBloodPressureScore(value) {
+        if (value <= 90 || value >= 181) {
+            return 3;
+        } else if ((value >= 91 && value <= 100) || (value >= 161 && value <= 180)) {
+            return 2;
+        } else if ((value >= 101 && value <= 110) || (value >= 141 && value <= 160)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    // Função para calcular a pontuação da Frequência Respiratória
+    function calculateBreathRateScore(value) {
+        if (value <= 8 || value >= 31) {
+            return 3;
+        } else if ((value >= 9 && value <= 11) || (value >= 21 && value <= 30)) {
+            return 2;
+        } else if ((value >= 12 && value <= 20)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Função para calcular a pontuação da Saturação do Oxigênio
+    function calculateOxygenSaturationScore(value) {
+        if (value <= 91) {
+            return 3;
+        } else if (value >= 92 && value <= 93) {
+            return 2;
+        } else if (value >= 94 && value <= 95) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
     useEffect(() => {
-        onScoreChange(score);
-    }, [score]);
+        const totalScore =
+            calculateTemperatureScore(value) +
+            calculateHeartRateScore(value) +
+            calculateBloodPressureScore(value) +
+            calculateBreathRateScore(value) +
+            calculateOxygenSaturationScore(value);
+
+        console.log(totalScore);
+
+        onScoreChange(totalScore);
+    }, [value, temperatureScore, heartRateScore, bloodPressureScore, breathRateScore, oxygenSaturationScore]);
+
+
 
     function handleChange(event) {
         const inputValue = event.target.value;
@@ -50,8 +118,12 @@ function NumberField({ id, onScoreChange }) {
             setState(determineState(hasNumbers));
         }
 
-        calculateTemperatureScore(numericValue);
-        setValue(numericValue);
+        setTemperatureScore(calculateTemperatureScore(inputValue));
+        setHeartRateScore(calculateHeartRateScore(inputValue));
+        setBloodPressureScore(calculateBloodPressureScore(inputValue));
+        setBreathRateScore(calculateBreathRateScore(inputValue));
+        setOxygenSaturationScore(calculateOxygenSaturationScore(inputValue));
+        setValue(inputValue);
     }
 
     function handleBlur() {
--- src/components/layouts/Form.jsx
+++ src/components/layouts/Form.jsx
@@ -5,50 +5,77 @@ import NumberField from "../inputs/InputField";
 
 export default function Form({onTotalScoreChange}) {
     const [temperatureScore, setTemperatureScore] = useState(0);
+    const [heartRateScore, setHeartRateScore] = useState(0);
+    const [bloodPressureScore, setBloodPressureScore] = useState(0);
+    const [breathRateScore, setBreathRateScore] = useState(0);
+    const [oxygenSaturationScore, setOxygenSaturationScore] = useState(0);
 
     function handleTemperatureScoreChange(score) {
         setTemperatureScore(score);
         onTotalScoreChange(calculateTotalScore());
     }
 
-    function calculateTotalScore () {
-        return temperatureScore;
+    function handleHearRateScoreChange(score) {
+        setHeartRateScore(score);
+        onTotalScoreChange(calculateTotalScore());
     }
 
-    const renderNumberField = (id, label, hint) => (
-        <FormField label={label} hint={hint}>
-            <NumberField id={id} onScoreChange={handleTemperatureScoreChange} />
-            <h2>{temperatureScore}</h2>
-        </FormField>
-    );
+    function handleBloodPressureScoreChange(score) {
+        setBloodPressureScore(score);
+        onTotalScoreChange(calculateTotalScore());
+    }
+
+    function handleBreathRateScoreChange(score) {
+        setBreathRateScore(score);
+        onTotalScoreChange(calculateTotalScore());
+    }
+
+    function handleOxygenSaturarionScoreChange(score) {
+        setOxygenSaturationScore(score);
+        onTotalScoreChange(calculateTotalScore());
+    }
+
+    function calculateTotalScore () {
+        return (
+            temperatureScore + heartRateScore + bloodPressureScore + breathRateScore + oxygenSaturationScore
+        );
+    }
+
+    const suplementationOptions = [
+        { id: "yes", text: "Sim" },
+        { id: "no", text: "Não" },
+    ]
+
+    const consciousnessLevels = [
+        { id: "typical", text: "Típico" },
+        { id: "reduced", text: "Reduzido" },
+    ]
 
     return (
         <form id="calculator-form">
-            {renderNumberField("temperature", "Temperatura (ºC)", "Digite apenas números.")}
-            {renderRadioGroup(
-                "consciousness-level",
-                "Nível de consciência",
-                "Selecione uma opção.",
-                [
-                    { id: "typical", text: "Típico" },
-                    { id: "reduced", text: "Reduzido" },
-                ]
-            )}
+            <FormField label="Temperatura (ºC)" hint="Digite apenas números.">
+                <NumberField id="temperature" onScoreChange={handleTemperatureScoreChange} />
+            </FormField>
+            <FormField label="Nível de consciência" hint="Selecione uma opção.">
+                <RadioGroup name="conciusness-level" options={consciousnessLevels} />
+            </FormField>
             <hr />
-            {renderNumberField("heart-rate", "Frequência cardíaca (bpm)", "Digite apenas números.")}
-            {renderNumberField("blood-pressure", "Pressão Arterial Sistólica (mmHg)", "Digite apenas números.")}
+            <FormField label="Frequêcia cardíaca (bpm)" hint="Digite apenas números.">
+                <NumberField id="heart-rate" onScoreChange={handleHearRateScoreChange} />
+            </FormField>
+            <FormField label="Pressão Arterial Sistótlica" hint="Digite apenas números.">
+                <NumberField id="blood-pressure" onScoreChange={handleBloodPressureScoreChange} />
+            </FormField>
             <hr />
-            {renderNumberField("breath-rate", "Frequência respiratória (irpm)", "Digite apenas números.")}
-            {renderNumberField("oxygen-saturation", "Saturação do O2 (%)", "Digite apenas números.")}
-            {renderRadioGroup(
-                "oxygen-supplementation",
-                "Usando suplementação O2?",
-                "Selecione uma opção.",
-                [
-                    { id: "yes", text: "Sim" },
-                    { id: "no", text: "Não" },
-                ]
-            )}
+            <FormField label="Frequência respiratória (irpm)" hint="Digite apenas números.">
+                <NumberField id="breath-rate" onScoreChange={handleBreathRateScoreChange} />
+            </FormField>
+            <FormField label="Saturação do o2 (%)" hint="Digite apenas números.">
+                <NumberField id="oxygen-saturation" onScoreChange={handleOxygenSaturarionScoreChange} />
+            </FormField>
+            <FormField label="Usando suplementação de O2?" hint="Selecione uma opção.">
+                <RadioGroup name="oxygen-suplementation" options={suplementationOptions} />
+            </FormField>
         </form>
     );
 }
