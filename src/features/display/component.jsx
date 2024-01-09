c404c247612ad66e0faf81b3bb03b7d05aa06d12
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2024-01-08 23:32:59 -0300

    feat(display): add classification description

--- src/features/display/component.jsx
+++ src/features/display/component.jsx
@@ -8,6 +8,12 @@ const CLASSIFICATION = {
     red: "alto",
 };
 
+const DESCRIPTION = {
+    green: "Deve ser avaliado por profissional de enfermagem com habilidades para decidir se há necessidade de aumentar a frequência do monitoramento e/ou escalonamento de cuidados clínicos",
+    yellow: "Avaliação urgente por um clínico qualificado com competências na avaliação de doenças agudas.",
+    red: "Avaliação de emergência por médico instensivista, com possível transferência do paciente para uma área de cuidados críticos."
+};
+
 function Display({ newsScore }) {
     const { defineRiskLevel } = useDisplay();
     const riskLevel = defineRiskLevel(newsScore);
@@ -20,6 +26,7 @@ function Display({ newsScore }) {
                     Paciente com <span>{CLASSIFICATION[riskLevel]}</span> risco
                     de deterioração clínica.
                 </h2>
+                <p className="card-section__description">{DESCRIPTION[riskLevel]}</p>
             </div>
         </CardSection>
     );
